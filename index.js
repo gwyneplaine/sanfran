82b8313b08abd6abfa4373cf3dcf0112239ee315
Charles Lee <clee@atlassian.com>
2019-02-26 17:27:15 +1100

    refactored converters to allow dependency injection, added CLI-y things, and initial hardcoded package iteration logic

--- index.js
+++ index.js
@@ -1,64 +1,112 @@
+#!/usr/bin/env node
 const parser = require('@babel/parser');
 const traverse = require('@babel/traverse').default;
 const types = require('@babel/types');
 const fs = require('fs');
 const path = require('path');
-const file = fs.readFileSync(path.resolve(__dirname, './component-tokens.js'), 'utf-8');
-let ast = parser.parse(file, {
-  sourceType: 'module',
-});
 
-let cache = {};
-
-
-function buildPropertyMap (key, value) {
-  if (cache[key]) {
-    cache[key] = cache[key].add(value);
-  } else {
-    cache[key] = new Set([value]);
+class PropertyMap {
+  constructor () {
+    this.propertyMap = {};
   }
-};
-
-function resolveToken (value) {
-  return Object.keys(cache).find((key) => (cache[key].has(value)));
-};
-
-function convertToArray (cache) {
-  return Object.keys(cache).reduce((acc, curr) => {
-    acc[curr] = Array.from(cache[curr]);
-    return acc;
-  }, {});
-};
-
-function getLiteralIdentifier (path, context) {
-  let parent = path.get('parent');
-  if (parent.parentPath.parentKey === 'init') {
-    let key = parent.parentPath.container.id.name;
-    buildPropertyMap(path.node.value, key);
+  build (key, value) {
+    if (this.propertyMap[key]) {
+      this.propertyMap[key] = this.propertyMap[key].add(value);
+    } else {
+      this.propertyMap[key] = new Set([value]);
+    }
+  }
+  resolveProperty (value) {
+    return Object.keys(this.propertyMap).find((key) => (this.propertyMap[key].has(value)));
+  }
+  getExportableMap () {
+    return Object.keys(this.propertyMap).reduce((acc, curr) => {
+      acc[curr] = Array.from(this.propertyMap[curr]);
+      return acc;
+    }, {});
   }
 }
 
-traverse(ast, {
-  Identifier: function (path) {
+let propertyMap = new PropertyMap();
+
+converterFactory = (name) => {
+  let converters = {};
+
+  function getLiteralIdentifier (path, context) {
     let parent = path.get('parent');
     if (parent.parentPath.parentKey === 'init') {
-      let token = resolveToken(path.node.name);
-      buildPropertyMap(token, parent.parentPath.container.id.name);
+      let key = parent.parentPath.container.id.name;
+      propertyMap.build(path.node.value, `${name}_${key}`);
     }
-  },
-  StringLiteral: getLiteralIdentifier,
-  NumericLiteral: getLiteralIdentifier,
-  VariableDeclarator: function (path) {
-    let id = path.get('id');
-    let init = path.get('init');
-  },
-  MemberExpression: function (path) {
-    let object = path.get('object').node.name;
-    let property = path.get('property')
-    let binding = `${object}.${property.node.name}`;
-    let assignee = path.parentPath.node.id.name;
-    buildPropertyMap(`${object}.${property}`, assignee);
-  },
-});
+  }
 
-fs.writeFileSync(path.resolve(__dirname, './data.json'), JSON.stringify(convertToArray(cache), null, 1));
+  converters.Identifier = function (path, thing) {
+    let parent = path.get('parent');
+    if (parent.parentPath.parentKey === 'init') {
+      const token = propertyMap.resolveProperty(path.node.name);
+      propertyMap.build(token, `${name}_${parent.parentPath.container.id.name}`);
+    }
+  };
+  converters.StringLiteral = getLiteralIdentifier;
+  converters.NumericLiteral = getLiteralIdentifier;
+  converters.MemberExpression = function (path) {
+    const object = path.get('object').node.name;
+    const property = path.get('property').node.name;
+    const assignee = path.parentPath.node.id.name;
+    propertyMap.build(`${object}.${property}`, `${name}_${assignee}`);
+  };
+
+  return converters;
+};
+
+async function componentAudit (component) {
+  // access a list of components
+  // queue a Promise.all that loads in the
+  // looad the component name from the package.json
+
+  try {
+    const componentPath = path.resolve(process.cwd(), `./packages/core/${component}`);
+    const componentTokensPath = path.resolve(componentPath, './src/component-tokens.js');
+    if (!fs.existsSync(componentPath)) {
+      console.error(`WARNING: ${componentPath} is invalid, moving on`);
+      return;
+    }
+    if (!fs.existsSync(componentTokensPath)) {
+      console.error(`WARNING ${componentTokensPath} is invalid, moving on`);
+      return;
+    }
+    const componentName = await require(path.resolve(componentPath, './package.json')).name;
+    const file = fs.readFileSync(componentTokensPath, 'utf-8');
+    let ast = parser.parse(file, {
+      sourceType: 'module',
+    })
+    traverse(ast, converterFactory(componentName));
+  } catch (e) {
+    console.trace(e);
+  }
+
+}
+async function run () {
+  // access a list of components
+  // queue a Promise.all that loads in the
+  // looad the component name from the package.json
+  const componentList = [
+    'button',
+    'checkbox',
+    'radio',
+    'inline-edit',
+    'inline-message',
+    'section-message',
+    'flag',
+    'select',
+    'banner',
+    'badge',
+    'lozenge',
+    'css-reset'
+  ];
+  await Promise.all(componentList.map(componentAudit));
+  const exportableMap = propertyMap.getExportableMap();
+  fs.writeFileSync(path.resolve(__dirname, './data.json'), JSON.stringify(exportableMap, null, 1));
+}
+
+run();
